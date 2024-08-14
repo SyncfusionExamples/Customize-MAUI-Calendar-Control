df00073c7da3d8692145ebffb12044bea88eab3d
snehakPV <sneha.sf4154@gmail.com>
2024-08-14 12:03:11 +0530

    Prepared the sample

---
+++ CalendarSample/CalendarSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CalendarSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CalendarSample/CalendarSample/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.Maui.Calendar;
+
+namespace CalendarSample
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            calendar.MonthView.SpecialDayPredicate = (date) =>
+            {
+                if (date.Date == DateTime.Now.AddDays(2).Date)
+                {
+                    CalendarIconDetails iconDetails = new CalendarIconDetails();
+                    iconDetails.Icon = CalendarIcon.Heart;
+                    iconDetails.Fill = Colors.Red;
+                    return iconDetails;
+                }
+                return null;
+            };
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            calendar.IsOpen = true;
+        }
+
+    }
+
+}
